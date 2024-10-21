5dbda964273efbc00a1a6c04686de4a1a8158807
Anas Farooq <anasf1905@gmail.com>
2024-10-21 11:46:07 +0500

    modified:   .gitignore 	modified:   README.md 	new file:   next.config.js 	deleted:    next.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	deleted:    postcss.config.mjs 	new file:   public/github-icon.svg 	deleted:    public/images/AboutMe.png 	new file:   public/images/about-image.png 	new file:   public/images/hero-image.png 	deleted:    public/images/profile.png 	deleted:    public/images/projects/02.png 	deleted:    public/images/projects/03.png 	new file:   public/images/projects/1.png 	new file:   public/images/projects/2.png 	new file:   public/images/projects/3.png 	new file:   public/images/projects/4.png 	new file:   public/images/projects/5.png 	renamed:    public/images/projects/01.png -> public/images/projects/6.png 	new file:   public/linkedin-icon.svg 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   src/app/api/send/route.js 	modified:   src/app/components/AboutSection.jsx 	new file:   src/app/components/AchievementsSection.jsx 	new file:   src/app/components/EmailSection.jsx 	new file:   src/app/components/Footer.jsx 	modified:   src/app/components/HeroSection.jsx 	modified:   src/app/components/MenuOverlay.jsx 	modified:   src/app/components/NavLink.jsx 	modified:   src/app/components/Navbar.jsx 	modified:   src/app/components/ProjectCard.jsx 	new file:   src/app/components/ProjectTag.jsx 	modified:   src/app/components/ProjectsSection.jsx 	modified:   src/app/components/TabButton.jsx 	deleted:    src/app/fonts/GeistMonoVF.woff 	deleted:    src/app/fonts/GeistVF.woff 	modified:   src/app/globals.css 	modified:   src/app/layout.js 	modified:   src/app/page.js 	modified:   tailwind.config.js

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import Navlink from './Navlink';
+import React from "react";
+import NavLink from "./NavLink";
 
 const MenuOverlay = ({ links }) => {
   return (
-    <ul className='flex flex-col py-4 items-center'>{links.map((link, index) => (
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, index) => (
         <li key={index}>
-            <Navlink href={link.path} title={link.title}/> 
+          <NavLink href={link.path} title={link.title} />
         </li>
-        ))} 
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default MenuOverlay
+export default MenuOverlay;
