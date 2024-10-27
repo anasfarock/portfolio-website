97cb3cf61108cfc5c075eb2d34a7b7d5a00900c3
Anas Farooq <anasf1905@gmail.com>
2024-10-27 11:28:57 +0500

    modified:   src/app/components/AchievementsSection.jsx

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -12,7 +12,7 @@ const AnimatedNumbers = dynamic(
 const achievementsList = [
   {
     metric: "Projects",
-    value: "100",
+    value: "10",
     postfix: "+",
   },
   {
@@ -21,8 +21,8 @@ const achievementsList = [
     value: "100,000",
   },
   {
-    metric: "Awards",
-    value: "7",
+    metric: "Certifications",
+    value: "2",
   },
   {
     metric: "Years",
