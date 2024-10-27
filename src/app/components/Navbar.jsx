06513a31f16d14400d16fff896233c7cce496257
Anas Farooq <anasf1905@gmail.com>
2024-10-27 12:27:45 +0500

    modified:   src/app/components/Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -26,12 +26,13 @@ const Navbar = () => {
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link
-          href={"/images/logo.png"}
-          className="text-2xl md:text-5xl text-white font-semibold"
-        >
-          Anas Farooq
-        </Link>
+      <Link
+  href="/"
+  className="text-2xl md:text-5xl text-white font-semibold"
+>
+  <img src="/images/logo.png" alt="Anas Farooq Logo" className="h-8 w-8 md:h-10 md:w-10" />
+</Link>
+
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
