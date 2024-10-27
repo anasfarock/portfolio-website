83f8c778792e1114b8c3978cd031823a1549e838
Anas Farooq <anasf1905@gmail.com>
2024-10-27 13:16:41 +0500

    modified:   src/app/components/HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#6B8E23] to-[#E8D8B5]">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
@@ -38,13 +38,13 @@ const HeroSection = () => {
           <div>
             <Link
               href="https://www.linkedin.com/in/anasfarock/"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#5A7D2A] to-[#b19a7e] hover:bg-slate-200 text-white"
             >
               Hire Me
             </Link>
             <Link
-              href="/Curriculum_Vitae.pdf"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#5A7D2A] to-[#b19a7e] hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
