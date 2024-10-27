4bebd1c29e98a1af464f2981c476dccf7bfea0d4
Anas Farooq <anasf1905@gmail.com>
2024-10-27 10:57:04 +0500

    new file:   public/images/file.png 	modified:   src/app/components/HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -59,15 +59,18 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+        <div className="col-span-5 flex justify-center">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] overflow-hidden relative">
+            <Image 
+              src="/images/file.png"
+              alt="Profile Picture"
+              className="absolute top-1/2 left-1/2 w-full h-full object-cover transform -translate-x-1/2 -translate-y-1/2"
+              width={400}
+              height={400}
             />
           </div>
+        </div>
+
         </motion.div>
       </div>
     </section>
