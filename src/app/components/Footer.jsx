ee4062a0aaa1ec72d4f7992a210e0848a88c1836
Anas Farooq <anasf1905@gmail.com>
2024-12-21 17:18:35 +0500

    modified:   src/app/components/Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+      <div className="container p-12 flex justify-between">
         <img 
           src="/images/logo.png" 
           alt="Anas Farooq Logo" 
