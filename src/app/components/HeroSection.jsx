86e6477f3b50d3d195a3b3fac5020c3fe65c3653
Anas Farooq <anasf1905@gmail.com>
2025-03-18 19:38:46 +0500

    Hero Section and Profile Updated

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -61,7 +61,7 @@ const HeroSection = () => {
         <div className="col-span-5 flex justify-center">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] overflow-hidden relative">
             <Image 
-              src="/images/file.png"
+              src="/images/file.jpeg"
               alt="Profile Picture"
               className="absolute top-1/2 left-1/2 w-full h-full object-cover transform -translate-x-1/2 -translate-y-1/2"
               width={400}
