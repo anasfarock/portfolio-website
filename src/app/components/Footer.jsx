61a08e196ac214437ff05f0cc065d5a7d5ab7d4e
Anas Farooq <anasf1905@gmail.com>
2025-03-18 19:14:50 +0500

    Footer Updated

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <img 
-          src="/images/logo.png" 
-          alt="Anas Farooq Logo" 
-          className="w-[150px] h-auto md:w-[200px]" 
+        <Image
+          src="/images/logo.png"
+          alt="Anas Farooq Logo"
+          width={150} // Base width in pixels
+          height={50} // Adjust this based on your logo’s aspect ratio
+          className="md:w-[200px] h-auto"
         />
         <p className="text-slate-600">All rights reserved.</p>
       </div>
