f96a35eb90bdbb3ed32c94391ff855e6be473fd3
Muhammad Anas Farooq <anasf1905@gmail.com>
2025-08-29 20:25:25 +0500

    Removed Contact Section

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,6 @@ export default function Home() {
         <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
-        // <EmailSection/>
       </div>
       <Footer />
     </main>
