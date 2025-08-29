afdd0ab2d305c18229b4eb6a6297aa159ba846d9
Muhammad Anas Farooq <anasf1905@gmail.com>
2025-08-29 20:24:22 +0500

    Commented Contact Section

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
         <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
-        <EmailSection/>
+        // <EmailSection/>
       </div>
       <Footer />
     </main>
