0e5e28564de7bcf18b30480b2873dc1f670c323e
anasfarock <anasf1905@gmail.com>
2025-10-08 09:14:24 +0500

    updated project section title color issue

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl, showLivePreview }) => {
+const ProjectCard = ({
+  imgUrl,
+  title,
+  description,
+  gitUrl,
+  previewUrl,
+  showLivePreview,
+}) => {
   return (
     <div className="relative group">
       <div className="h-52 md:h-72 bg-[#181818] rounded-t-xl overflow-hidden">
@@ -14,11 +21,11 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl, showLiveP
               title={`${title} Preview`}
               className="w-full h-full border-0 outline-0"
               style={{
-                width: '400%',
-                height: '400%',
-                transform: 'scale(0.25)',
-                transformOrigin: 'top left',
-                overflow: 'hidden'
+                width: "400%",
+                height: "400%",
+                transform: "scale(0.25)",
+                transformOrigin: "top left",
+                overflow: "hidden",
               }}
               scrolling="no"
             />
@@ -31,28 +38,28 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl, showLiveP
             style={{ backgroundImage: `url(${imgUrl})` }}
           ></div>
         )}
-        
+
         {/* Overlay with action buttons */}
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
           <Link
             href={gitUrl}
-            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-[#ffffff] group/link"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ffffff]" />
           </Link>
           <Link
             href={previewUrl}
-            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-[#ffffff] group/link"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ffffff]" />
           </Link>
         </div>
       </div>
-      <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
+      <div className="text-[#ffffff] rounded-b-xl bg-[#181818] py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
